1db06c8b2c57f1c58da9f43c1d8581bd904dc62f
Nicolantonio Vignola <n.vignola@holidaypirates.com>
2017-10-01 15:13:54 +0200

    Add basic configuration and files

---
+++ index.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+
+body.innerHTML = `
+    <h1>
+        It Works!
+    </h1>
+`;
+
+console.log('Delete me');
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+ var path = require('path');
+ var webpack = require('webpack');
+
+ module.exports = {
+     entry: './index.js',
+     output: {
+         path: path.resolve(__dirname, 'build'),
+         filename: 'index.bundle.js'
+     },
+     module: {
+         loaders: [
+             {
+                 test: /\.js$/,
+                 loader: 'babel-loader',
+                 query: {
+                     presets: ['env']
+                 }
+             }
+         ]
+     },
+     stats: {
+         colors: true
+     },
+     devtool: 'source-map'
+ };
